e5dbca29f5bcb3c937fca55133b41af2135f3755
heshashawky <heshamwebdeve@gmail.com>
2016-03-30 18:07:06 +0200

    Hesham Shal Kato

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function() {
-  
+$(document).ready(function() {
+
 
    $('#carouselHacked').carousel();
-  
-	//this code is for the gmap
-	 var map = new GMaps({
+
+    //this code is for the gmap
+     var map = new GMaps({
         el: '#map',
         lat: -12.043333,
         lng: -77.028333
@@ -14,17 +14,17 @@ jQuery(document).ready(function() {
       //this code is for smooth scroll and nav selector
             $(document).ready(function () {
               $(document).on("scroll", onScroll);
-              
+
               //smoothscroll
               $('a[href^="#"]').on('click', function (e) {
                   e.preventDefault();
                   $(document).off("scroll");
-                  
+
                   $('a').each(function () {
                       $(this).removeClass('active');
                   })
                   $(this).addClass('active');
-                
+
                   var target = this.hash,
                       menu = target;
                   $target = $(target);
@@ -37,22 +37,9 @@ jQuery(document).ready(function() {
               });
           });
 
-          function onScroll(event){
-              var scrollPos = $(document).scrollTop();
-              $('.navbar-default .navbar-nav>li>a').each(function () {
-                  var currLink = $(this);
-                  var refElement = $(currLink.attr("href"));
-                  if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                      $('.navbar-default .navbar-nav>li>a').removeClass("active");
-                      currLink.addClass("active");
-                  }
-                  else{
-                      currLink.removeClass("active");
-                  }
-              });
-          }
-     
-     
+
+
+
      //this code is for animation nav
      jQuery(window).scroll(function() {
         var windowScrollPosTop = jQuery(window).scrollTop();
@@ -66,11 +53,20 @@ jQuery(document).ready(function() {
           jQuery(".header").css({"background": "transparent",});
            jQuery(".top-header img.logo").css({"margin-top": "-12px", "margin-bottom": "25px"});
            jQuery(".navbar-default").css({"margin-top": "12px", "margin-bottom": "0"});
-          
+
         }
      });
 
-      
-     
+    $('#m1').click(function () {
+        $('#mom').fadeToggle("fast");
+        $('#minor').fadeOut("fast");
+
+    });
+
+    $('#m2').click(function () {
+        $('#minor').fadeToggle("fast");
+        $('#mom').fadeOut("fast");
+    });
+
 
 });
